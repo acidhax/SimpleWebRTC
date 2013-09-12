879e76491ad2d3153c9d7c4d359f365c5ecd6262
= <mail@matbee.com>
2013-09-12 13:22:50 -0400

    changes

--- index.js
+++ index.js
@@ -167,8 +167,6 @@ app.get('/extension-connect', function(req, res) {
   });
 });
 
-
-
 wh.addNamespace('/service');
 wh.setPath(wormholeExternalProtocol + "://"+os.hostname()+":"+wormholeExternalPort+"/service/connect.js");
 wh.on("live", function (cb) {
