d099683ef7815d7e8de60132a827c18e5c2ede8b
Mathieu Gosbee <mail@matbee.com>
2013-09-25 19:08:20 -0400

    .

--- public/js/AccountSearch.js
+++ public/js/AccountSearch.js
@@ -24,7 +24,7 @@ AccountSearch.prototype.search = function(string, cb) {
 AccountSearch.prototype.remove = function (el) {
 	var found = false;
 	for (var i = 0; i < this.accounts.length && !found; i++) {
-		if (this.accounts._el = el) {
+		if (this.accounts[i]._el == el) {
 			this.accounts.splice(i,1);
 			found = true;
 		}
